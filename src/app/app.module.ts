0d41c56b3053bebe9cdd714f08606cd31dbdd049
amiloszewicz <miloszewicz.adam@gmail.com>
2021-03-28 18:41:14 +0200

    refactor module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { QuoteComponent } from './quote/quote.component';
+import { ButtonComponent } from './button/button.component';
+import { InstructionComponent } from './instruction/instruction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    QuoteComponent,
+    ButtonComponent,
+    InstructionComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
